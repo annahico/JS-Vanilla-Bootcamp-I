43c863e2edeb781a219c5407c801f69a4d3c537b
annahico <annahico@gmail.com>
2024-03-27 20:12:51 +0100

    acabando reto array

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// // DATOS COMPLETOS o ARRAY
+
+// let peliculas = ["el señor de los anillos", "harry potter", "star wars", "parque jurasico", "la vida de brian", "orgullo y prejuicio"] //esto es un ARRAY
+
+// console.log(peliculas[0]); //permite guardar una matriz de datos
+
+// let hobbiesJuanPablo = ["correr", "futbol", "lol", "cocinar"]
+
+// hobbiesJuanPablo[2];
+
+// ///////////////////////////////////////
+
+// let pablo = {
+//     nombre: "Pablo",
+//     edad: 30,
+//     altura: 1.85,
+//     hobbies: {
+//         hobbie1: "futbol",
+//         hobbie2: "xbox",
+//         hobbie3: "bicicleta",
+//     }
+// }
+
+// console.log(pablo.hobbies.hobbie3)
+
+// //MANEJO DEL ARRAY
+
+// let videojuegosDani = ["gta5", "rdr", "counter"];
+
+// console.log("primero",videojuegosDani);
+
+// videojuegosDani.push("rocket league"); //introduce un elemento al final del ARRAY
+
+// console.log("segundo",videojuegosDani);
+
+// // Prueba de manejos de ARRAY
+
+let verduras = ["colifor", "lechuga", "apio", "cebolla", "pimiento", "puerro"];
+
+console.log("primero",verduras);
+
+verduras.push("espinacas");
+
+console.log("segundo", verduras);
+
+verduras.pop("espinacas");
+
+console.log("tercero",verduras);
+
+verduras.shift("coliflor");
+
+console.log("cuarto", verduras);
+
+verduras.unshift("tomate");
+
+console.log("quinto", verduras);
+
+
+
+
+
